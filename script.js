f29b401327ec54db5f337de6aa95c98aaa53c219
PROBOEDU <probodeveloper@gmail.com>
2024-10-25 15:28:37 +0900

    Test2

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
- // 첫 번째 버튼에 대한 이벤트 리스너 추가
- document.getElementById('btn1').addEventListener('click', function() {
-    alert('첫 번째 버튼이 눌렸습니다!');
+// 클릭된 버튼을 저장하는 변수
+let clickedButton = "";
+
+// 첫 번째 버튼에 대한 이벤트 리스너 추가
+document.getElementById('btn1').addEventListener('click', function() {
+    clickedButton = "1번";
+    alert(clickedButton);
+    console.log(clickedButton); // 콘솔에 현재 저장된 값을 출력 (디버깅용)
 });
 
 // 두 번째 버튼에 대한 이벤트 리스너 추가
 document.getElementById('btn2').addEventListener('click', function() {
-    alert('두 번째 버튼이 눌렸습니다!');
+    clickedButton = "2번";
+    alert(clickedButton);
+    console.log(clickedButton); // 콘솔에 현재 저장된 값을 출력 (디버깅용)
 });
